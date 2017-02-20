30946c363e30cf6dd55dfe632760c16a76e2e12a
vgorcinschi <vasigorc@gmail.com>
2017-02-20 08:23:42 -0500

    Arquillian issues.

---
+++ src/main/java/com/vgorcinschi/rimmanew/cdi/UserPreferencies.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vgorcinschi.rimmanew.cdi;
+
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Optional;
+import static java.util.Optional.ofNullable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+/**
+ *
+ * @author vgorcinschi
+ */
+@Named
+@SessionScoped
+public class UserPreferencies implements Serializable {
+
+    private Locale currentLocale;
+
+    public UserPreferencies() {
+        //locale is initiated to French but may be overriden in the
+        //post-constructed method
+        currentLocale = new Locale("fr");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        /**
+         * if a user specific locale is provided in the browser
+         * we should be setting it here
+         */
+        Optional<ExternalContext> optContext = ofNullable(FacesContext.getCurrentInstance()
+                .getExternalContext());
+        if (optContext.isPresent()) {
+            Optional<Locale> userLocale = ofNullable(optContext.get().getRequestLocale());
+            if (userLocale.isPresent()) {
+                currentLocale = userLocale.get();
+            }
+        }
+    }
+
+    public Locale getCurrentLocale() {
+        return currentLocale;
+    }
+
+    public void setCurrentLocale(Locale currentLocale) {
+        this.currentLocale = currentLocale;
+    }
+    
+}
---
+++ src/test/java/com/vgorcinschi/rimmanew/cdi/UserPreferenciesTests.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vgorcinschi.rimmanew.cdi;
+
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Ideally we would be able to create an outside container, arquillian based,
+ * test to test various aspects of the UserPreferencies CDI bean
+ *
+ * @author vgorcinschi
+ */
+@RunWith(Arquillian.class)
+public class UserPreferenciesTests {
+
+    public UserPreferenciesTests() {
+    }
+
+    //humble attempt to mock glassfish only with the UserPreferencies bean inside
+    @Deployment
+    public static JavaArchive createDeployment() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
+                .addClass(UserPreferencies.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        System.out.println(jar.toString(true));
+        return jar;
+    }
+    
+    @Inject
+    UserPreferencies preferencies;
+
+    @Test
+    public void defaultLocaleTest() {
+        assertTrue("The default locale should be \"fr\"",
+                "fr".equalsIgnoreCase(preferencies.getCurrentLocale().getCountry()));
+    }
+}
