3f1ba3c3d08658c3405b7a1a7bee7e2f6946352e
Vasile Gorcinschi <vasigorc@gmail.com>
2018-04-16 10:21:33 -0400

    Change default username and password for DB connection

---
+++ src/main/resources/DefaultDatabaseAndUser.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  vgorcinschi
+ * Created: Apr 16, 2018
+ */
+
+create database beauty_shop;
+
+create user 'rimmanew'@'localhost' identified by 'd0InzJYLSIL0gbZ2018!';
+
+grant all privileges on beauty_shop.* to 'rimmanew'@'localhost';
